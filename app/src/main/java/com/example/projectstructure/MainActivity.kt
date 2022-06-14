3d1c0ea4fc2ff8e06f1c92db055c751b7585b32d
Sofian Slimani <sofianslimani48@gmail.com>
2022-06-14 13:15:20 +0200

    first commit

---
+++ app/src/main/java/com/example/projectstructure/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.projectstructure
+
+import android.icu.lang.UCharacter.GraphemeClusterBreak.L
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.StringBuilder
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+
+}
